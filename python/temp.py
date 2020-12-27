c427be0c4b1526fe2bf49ed564f50d008288df85
Andrew Porter <mephisto83@hotmail.com>
2020-12-26 21:34:41 -0600

    Update temp.py

--- python/temp.py
+++ python/temp.py
@@ -1,9 +1,48 @@
 import bpy
 import bmesh
+import math
 from mathutils import Vector, Matrix
 from bmesh.types import BMVert
 from bmesh.types import BMFace
 
+class OverhangOutput:
+    def __init__(self, left, right, top, bottom):
+        self.left = left
+        self.right = right
+        self.top = top
+        self.bottom = bottom
+    def print(self):
+        print("-------------------- over hang --------------------- ")
+        print("left :  " + str(self.left))
+        print("right :  " + str(self.right))
+        print("top :  " + str(self.top))
+        print("bottom :  " + str(self.bottom))
+class PaneTrims:
+    def __init__(self, panes, trims):
+        self.trims = trims
+        self.panes = panes
+class CrossCutParameters:
+    def __init__(self, x_count, y_count, x_cross_thickness, y_cross_thickness):
+        self.x_count = x_count
+        self.y_count = y_count
+        self.x_cross_thickness = x_cross_thickness
+        self.y_cross_thickness = y_cross_thickness
+class DoorParameters:
+    def __init__(self, jam_widths=.1):
+        self.jam_widths = jam_widths
+class DoorJamResult:
+    def __init__(self, top, bottom, right, left, center):
+        self.top = top
+        self.bottom = bottom
+        self.right = right
+        self.left = left
+        self.center = center
+    def print(self):
+        print("left :  " + str(self.left))
+        print("right :  " + str(self.right))
+        print("top :  " + str(self.top))
+        print("bottom :  " + str(self.bottom))
+        print("center :  " + str(self.center))
 # make mesh
 class ExtrudeObjectOutput:
     def __init__(self, faces, existing_faces, new_face_indices):
@@ -72,7 +111,39 @@ class BuildingTools:
         existing_faces = [face.index for face in bm.faces]
         bm.to_mesh(new_object.data) 
         bm.free()
-        return res 
+        return res
+    @staticmethod
+    def extrudeFaces(new_object, height, faceindices):
+        bm = bmesh.new() 
+        depsgraph = bpy.context.evaluated_depsgraph_get()
+        scene = bpy.context.scene
+        bm.from_object(new_object, depsgraph)
+        # Get geometry to extrude 
+        bm.faces.ensure_lookup_table() 
+        bm.normal_update()
+        existing_faces = [face.index for face in bm.faces]
+        _face = bm.faces[faceindices[0]]
+        faces = [bm.faces[face] for face in faceindices]
+        # Extrude 
+        extruded = bmesh.ops.extrude_face_region(bm, geom=faces)
+        # Move extruded geometry 
+        translate_verts = [v for v in extruded['geom'] if isinstance(v, BMVert)] 
+        bmesh.ops.translate(bm, vec=_face.normal*height, verts=translate_verts)
+        # Delete original faces 
+        bmesh.ops.delete(bm, geom=faces, context='FACES') 
+        # Remove doubles 
+        bmesh.ops.remove_doubles(bm, verts=bm.verts, dist=0.001) 
+        # Update mesh and free Bmesh 
+        bm.faces.ensure_lookup_table() 
+        bm.normal_update() 
+        updated_faces = [v for v in extruded['geom'] if isinstance(v, BMFace)]
+        bm.faces.ensure_lookup_table() 
+        new_face_indices = [face.index for face in bm.faces if face.index > len(existing_faces)-1]
+        res = ExtrudeObjectOutput(updated_faces, existing_faces,new_face_indices)
+        existing_faces = [face.index for face in bm.faces]
+        bm.to_mesh(new_object.data) 
+        bm.free()
+        return res
     @staticmethod
     def scaleFaceInline(face, verts, scale_factor):
         if isinstance(face, bmesh.types.BMFace):
@@ -136,7 +207,23 @@ class BuildingTools:
         y = BuildingTools.scaling(height, sidewalk)
         bm.free()
         return BuildingTools.extrudeScale(new_object, Vector((x,y,0)), faceIndex, sidewalk)
-
+    @staticmethod
+    def scaleFace(new_object, sidewalk, faceIndex):
+        mesh = new_object.data
+        bm = bmesh.new() 
+        depsgraph = bpy.context.evaluated_depsgraph_get()
+        scene = bpy.context.scene
+        bm.from_object(new_object, depsgraph)
+        # Get geometry to extrude 
+        bm.faces.ensure_lookup_table() 
+        bm.normal_update()
+        face = bm.faces[faceIndex]
+        face_normal = face.normal
+        width, height = BuildingTools.getDimensions(new_object, faceIndex)
+        x = BuildingTools.scaling(width, sidewalk)
+        y = BuildingTools.scaling(height, sidewalk)
+        bm.free()
+        return BuildingTools.extrudeScale(new_object, face_normal, faceIndex, sidewalk)
     @staticmethod
     def scaling(x , d):
         return (((d * 2) / x) - 1) / -1
@@ -159,15 +246,12 @@ class BuildingTools:
         else:
             edges = [face.edges[1], face.edges[3]]
             edge_length = face.edges[1]
-        print(edge_length)
-        print(edge_length.verts[0].co)
         temp_a = (edge_length.verts[0].co[0], edge_length.verts[0].co[1], edge_length.verts[0].co[2])
         temp_b = (edge_length.verts[1].co[0], edge_length.verts[1].co[1], edge_length.verts[1].co[2])
         use_cap_endpoint = False
         res = bmesh.ops.subdivide_edges(bm, edges=edges, cuts=cuts)
         bm.faces.ensure_lookup_table() 
         bm.edges.ensure_lookup_table()
-        # print(res)
         previous_faces = [bm.faces[faceIndex]]
         new_face_indices = [face.index for face in bm.faces if face.index > len(existing_faces)-1]
         new_faces = [face for face in bm.faces if face.index > len(existing_faces)-1]
@@ -225,8 +309,6 @@ class BuildingTools:
                                             done = False
                                             break
                                             break
-                                        else:
-                                            print("")
         return res, res_faces
     @staticmethod
     def edgesAreParallel(edge1, edge2):
@@ -424,7 +506,6 @@ class BuildingTools:
         start = edge_length[0]
         end = edge_length[1]
         length = (start - end).length
-        print(length)
         endpoint = 0
         half_way = start.lerp(end, .5)
         half_length = (start - half_way).length
@@ -438,32 +519,114 @@ class BuildingTools:
             factor = (half_length - endpoint ) / half_length
             spot = half_way.lerp(start, factor)
             move =  spot - half_way
-        print("endpoint : " + str(endpoint))
-        print("factor : " + str(factor))
-        print("start : " + str(start))
-        print("half_way : " + str(half_way))
-        print("half_length " + str(half_length))
-        print("end : " + str(end))
-        print("spot : " + str(spot))
-        print("move : " + str(move))
+        #print("endpoint : " + str(endpoint))
+        #print("factor : " + str(factor))
+        #print("start : " + str(start))
+        #print("half_way : " + str(half_way))
+        #print("half_length " + str(half_length))
+        #print("end : " + str(end))
+        #print("spot : " + str(spot))
+        #print("move : " + str(move))
         BuildingTools.translateEdges(new_object, move, edges)
     @staticmethod 
     def cutPanel(new_object, faceIndex, vertical, relative_position):
         loopCutResult = BuildingTools.loopcutFace(new_object, faceIndex, 1, vertical)
         BuildingTools.moveEdgeAlong(new_object, relative_position, loopCutResult.edge_length, loopCutResult.new_edges)
         return loopCutResult
-    def cutOverHang(new_object, faceIndex, thickness):
+    @staticmethod
+    def cutOverHang(new_object, faceIndex, thickness, overhang):
+        left = faceIndex
         loopCutResult = BuildingTools.cutPanel(new_object, faceIndex, True, thickness)
-        loopCutResult.print()
+        right = loopCutResult.new_faces[0]
 
         loopCutResult = BuildingTools.cutPanel(new_object, faceIndex, True, -thickness)
-        loopCutResult.print()
-
-
-        loopCutResult = BuildingTools.cutPanel(new_object, loopCutResult.new_faces[0], False, -thickness)
-        loopCutResult.print()
-                    
-
+        top = loopCutResult.new_faces[0]
+        multiply_ = 1
+        if not overhang:
+            multiply_ = -1
+        loopCutResult = BuildingTools.cutPanel(new_object, loopCutResult.new_faces[0], False, multiply_ * thickness)
+        bottom = loopCutResult.new_faces[0]
+        res = OverhangOutput(left, right, top , bottom) 
+        return res                  
+    @staticmethod
+    def buildJam(new_object, faceIndex, doorParameters):
+        overhang = BuildingTools.cutOverHang(new_object, faceIndex, doorParameters.jam_widths, False)
+        center = overhang.bottom
+        top = overhang.top
+        right = overhang.right
+        left = overhang.left
+        loopCutResult = BuildingTools.cutPanel(new_object, overhang.bottom, False, doorParameters.jam_widths)
+        #extrudedFaces = BuildingTools.extrudeFaces(new_object, .02, [overhang.left, overhang.top, overhang.right])
+        bottom  = loopCutResult.new_faces[-1]
+        return DoorJamResult(top, bottom, right, left, center)
+    @staticmethod
+    def buildDoor(new_object, faceIndex, doorParameters):
+        overhang = BuildingTools.cutOverHang(new_object, faceIndex, doorParameters.jam_widths, False)
+        loopCutResult = BuildingTools.cutPanel(new_object, overhang.bottom, False, doorParameters.jam_widths)
+        BuildingTools.extrudeFaces(new_object, .02, [overhang.left, overhang.top, overhang.right, loopCutResult.new_faces[0]])
+    @staticmethod
+    def getDimensions(new_object, faceIndex):
+        bm = BuildingTools.buildBMesh()
+        face = bm.faces[faceIndex]
+        edges = face.edges
+        width = edges[0].calc_length()
+        height = edges[1].calc_length()
+        BuildingTools.close(bm, new_object)
+        return width, height
+    @staticmethod
+    def buildCrossCut(new_object, faceIndex, parameters):
+        width, height = BuildingTools.getDimensions(new_object, faceIndex)
+        panel_width = width / parameters.x_count
+        panel_height = height / parameters.y_count
+        x_cut_panel_face_ids = [faceIndex]
+        x_cross_thickness = parameters.x_cross_thickness
+        y_cross_thickness = parameters.y_cross_thickness
+        panes = []
+        trims = []
+        for i in range(parameters.x_count - 1):
+            lcr = BuildingTools.cutPanel(new_object, faceIndex, True, -panel_width)
+            #BuildingTools.cutPanel(new_object, lcr.new_faces[0], True, .01)
+            #BuildingTools.cutPanel(new_object, lcr.new_faces[0], False, .01)
+            #BuildingTools.cutPanel(new_object, lcr.new_faces[0], True, -.01)
+            #BuildingTools.cutPanel(new_object, lcr.new_faces[0], False, -.01)
+            #temp = BuildingTools.scaleFace(new_object, .0004, lcr.new_faces[0])
+            x_cut_panel_face_ids.append(lcr.new_faces[0])
+        y_cut_panel_faces_ids = []
+        for i in range(len(x_cut_panel_face_ids)):
+            id_rev = x_cut_panel_face_ids[ i]
+            lcr = BuildingTools.cutPanel(new_object, id_rev, True, x_cross_thickness/2)
+            trims.append(id_rev)
+            y_cut_panel_faces_ids.append(lcr.new_faces[0])
+            lcr = BuildingTools.cutPanel(new_object, lcr.new_faces[0], True, -x_cross_thickness/2)
+            trims.append(lcr.new_faces[0])
+        for i in range(len(y_cut_panel_faces_ids)):
+            id_rev = y_cut_panel_faces_ids[i]
+            pre_id_rev = id_rev
+            xy_cut_panel_face_ids = []
+            for j in range(parameters.y_count):
+                lcr = BuildingTools.cutPanel(new_object, id_rev, False, panel_height)
+                pre_id_rev = lcr.new_faces[0]
+                xy_cut_panel_face_ids.append(pre_id_rev)
+            for k in range(len(xy_cut_panel_face_ids)):
+                id_rev = xy_cut_panel_face_ids[k]
+                trims.append(id_rev)
+                lcr = BuildingTools.cutPanel(new_object, id_rev, False, -y_cross_thickness/2)
+                panes.append(lcr.new_faces[0])
+                lcr = BuildingTools.cutPanel(new_object, lcr.new_faces[0], False, y_cross_thickness/2)    
+                trims.append(lcr.new_faces[0])
+        #print("--------------trims #####################")
+        #print(trims)
+        #print("--------------panes #####################")
+        #print(panes)
+        return PaneTrims(panes, trims)
+        #print(x_cut_panel_face_ids)
+        #for f_index in range(len(x_cut_panel_face_ids)):
+        #    faceId = x_cut_panel_face_ids[f_index]
+        #    xy_cut_panel_face_ids.append(faceId) 
+        #    for i in range(parameters.y_count - 1):      
+        #        lcr = BuildingTools.cutPanel(new_object, faceId, False, -panel_height)
+        #        xy_cut_panel_face_ids.append(lcr.new_faces[0])
+        #print(xy_cut_panel_face_ids)
 size = 50
 floors = 10
 floor_height = 4.3
@@ -497,17 +660,61 @@ scaleResult.print()
 extrudeResult = BuildingTools.extrudeObject(new_object, height, scaleResult.topFace, "first_floor")
 extrudeResult.print()
 firstFloorInfo = extrudeResult
+levels = []
+for y in range(1):
+    loopCutResult = BuildingTools.loopCutAroundBuilding(new_object, firstFloorInfo.sideFaces[2], 1, False)
+    firstFloorInfoCut = loopCutResult
+    loopCutResult.print()
+    BuildingTools.moveEdgesTo(new_object, {'z': 3 * y + 4 }, loopCutResult.new_edges)
+    levels.append(firstFloorInfoCut)
+    temp = BuildingTools.loopCutAroundBuilding(new_object, firstFloorInfo.sideFaces[2], 1, False)
+    BuildingTools.moveEdgesTo(new_object, {'z': 3 * y + 5 }, temp.new_edges)
+    #BuildingTools.cutOverHang(new_object, firstFloorInfoCut.new_faces[3], 1, True)
+    #BuildingTools.cutOverHang(new_object, firstFloorInfoCut.new_faces[2], 1, False)
+    #BuildingTools.cutOverHang(new_object, firstFloorInfoCut.new_faces[1], 1, True)
+chuncks = []
+if True:
+    for t in range(len(levels)):
+        firstFloorInfoCut = levels[t]
+        jlength = len(firstFloorInfoCut.new_faces)
+        firstFloorInfoCut.print()
+        for j in range(jlength):
+            side_face = firstFloorInfoCut.new_faces[j]
+            width, height = BuildingTools.getDimensions(new_object, side_face)
+            #print("width: "+ str(width)+ ", height" + str(height))
+            #print(side_face)
+            face_cuts =  math.floor(width / 4)
+            #for i in range(face_cuts):
+                #width_step = width / face_cuts
+                #print("$$$$4$$$$$$$$$$$$$$$$$$$$")
+            firstFloorPanel_Cut = BuildingTools.loopcutFace(new_object, side_face, face_cuts, True)
+            firstFloorPanel_Cut.print()
+                #print("$$$$4$$$$$$$$$$$$$$$$$$$$")
+            for i in range(len(firstFloorPanel_Cut.new_faces)):
+                jam_result = BuildingTools.buildJam(new_object, firstFloorPanel_Cut.new_faces[i], DoorParameters(jam_widths=1))
+                #BuildingTools.buildJam(new_object, jam_result.center, DoorParameters(jam_widths=.2))
+                #print("$$$$4$$$$$$$$$$$$$$$$$$$$")
+                #jam_result.print()
+                # ccrulet = BuildingTools.buildCrossCut(new_object, jam_result.center, CrossCutParameters(3, 3, .1, .1))
+                #chuncks.append(jam_result.center)
+print(levels)
+for i in range(len(chuncks)):
+    center = chuncks[i]
+    #print(chuncks)
+    #print(chuncks[i])
+    #ccrulet = BuildingTools.buildCrossCut(new_object, center, CrossCutParameters(3, 3, .1, .1))
+#chuncks = sorted(chuncks)
 
-loopCutResult = BuildingTools.loopCutAroundBuilding(new_object, firstFloorInfo.sideFaces[2], 1, False)
-firstFloorInfoCut = loopCutResult
-loopCutResult.print()
-
-BuildingTools.moveEdgesTo(new_object, {'z': 4}, loopCutResult.new_edges)
-BuildingTools.cutOverHang(new_object, firstFloorInfoCut.new_faces[3], 1)
-BuildingTools.cutOverHang(new_object, firstFloorInfoCut.new_faces[2], 1)
-BuildingTools.cutOverHang(new_object, firstFloorInfoCut.new_faces[1], 1)
-BuildingTools.cutOverHang(new_object, firstFloorInfoCut.new_faces[0], 1)
-
+#for i in  range(2):    
+    #ccrulet = BuildingTools.buildCrossCut(new_object, jam_result.center, CrossCutParameters(3, 3, .1, .1))
+    #extrudedFaces = BuildingTools.extrudeFaces(new_object, .02, ccrulet.trims)
+#print(chuncks)
+#overhang = BuildingTools.cutOverHang(new_object, firstFloorInfoCut.new_faces[0], 1, False)
+#overhang2 = BuildingTools.cutOverHang(new_object, overhang.bottom, 1, False)
+#BuildingTools.extrudeFaces(new_object, 4, [overhang2.bottom])
+#BuildingTools.extrudeFaces(new_object, 3, [overhang2.left, overhang2.top, overhang2.right])
+#BuildingTools.extrudeFaces(new_object, 2, [overhang.left, overhang.top, overhang.right])
+#overhang.print()
 #loopCutResult = BuildingTools.loopcutFace(new_object, firstFloorInfoCut.new_faces[3], 1, True)
 #loopCutResult.print()
 #BuildingTools.moveEdgesTo(new_object, {'x': 88.181 - 2}, loopCutResult.new_edges)
